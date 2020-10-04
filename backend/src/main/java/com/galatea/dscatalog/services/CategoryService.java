f3ade6bdf3e75aac60deec6da09d0e17b63f9fb6
Rayssa Vilaca <cecilia_vilaca@outlook.com>
2020-10-03 22:12:21 -0300

    H2 database

---
+++ backend/src/main/java/com/galatea/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.galatea.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.galatea.dscatalog.entities.Category;
+import com.galatea.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+}
