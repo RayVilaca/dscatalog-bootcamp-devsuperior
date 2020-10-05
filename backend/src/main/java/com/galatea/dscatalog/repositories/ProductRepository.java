fe2b89ae8f37554432bd0178ab0ee845554b8fd1
Rayssa Vilaca <cecilia_vilaca@outlook.com>
2020-10-04 23:07:07 -0300

    Product repository, service, resource, GET ok

---
+++ backend/src/main/java/com/galatea/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.galatea.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.galatea.dscatalog.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
